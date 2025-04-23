209e3f9a4acbf765d585e15ee30fae349a0d7d0f
luis r <luisresendez_@hotmail.com>
2025-04-22 21:16:08 -0600

    waiting time for tests

--- cypress/e2e/sortedList.cy.js
+++ cypress/e2e/sortedList.cy.js
@@ -4,11 +4,13 @@ describe('Sorted list', () => {
     it('adds information to a list', () => {
         cy.visit('https://thelab.boozang.com/sortedList')
         cy.get('input').type('hola')
+        cy.wait(500)
         cy.get('.form_btn').click()
 
         cy.wait(1000)
         
         cy.get('input').type('mundo')
+        cy.wait(500)
         cy.get('.form_btn').click()
 
     })
